4b763d3297f688dbb0e459c67fdc8f432398bc5c
hectorcbs <hector.cbs86@gmail.com>
2016-09-11 16:30:05 +0200

    Update reducerStudents.py

--- reducerStudents.py
+++ reducerStudents.py
@@ -4,8 +4,8 @@
 
 # This MapReduce consists on getting the hours when the students are more active on the forum.
 # The first thing we have to do is to order by hour and to obtain an array of hours like this [05,12,12,14]
-# Then we have to prepare an array of popular hours because it could be that the student could have two or more
-# popular hours. Once we have the array of popular hours, we have only to print them with the associate student.
+# Then we have to prepare an array of popular hours because it could be the student could have two or more
+# popular hours. Once we have the array of popular hours, we have only to print it with the associate student.
 
 import sys
 
